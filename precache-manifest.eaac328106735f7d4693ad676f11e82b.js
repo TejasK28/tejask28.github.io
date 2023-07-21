35affe58b6ca8acaadc166f0795d80d4fd8bf93b
Tejas <tejaskandri28@gmail.com>
2023-07-21 17:21:38 -0400

    Updates

---
+++ precache-manifest.eaac328106735f7d4693ad676f11e82b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d9e93826953dc3606a386baadc42de",
+    "url": "/eportfolio/index.html"
+  },
+  {
+    "revision": "4da64181233f2de11d82",
+    "url": "/eportfolio/static/css/main.e080949b.chunk.css"
+  },
+  {
+    "revision": "99fdcc45bc37263a5d3c",
+    "url": "/eportfolio/static/js/2.9ad929f8.chunk.js"
+  },
+  {
+    "revision": "b792366b0bf70b8910d3ca85f53b1654",
+    "url": "/eportfolio/static/js/2.9ad929f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da64181233f2de11d82",
+    "url": "/eportfolio/static/js/main.91340a05.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/eportfolio/static/js/main.91340a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43458b04baab43a3408",
+    "url": "/eportfolio/static/js/runtime-main.6f5297aa.js"
+  },
+  {
+    "revision": "72cde5813503aff2994bcdbd00dea887",
+    "url": "/eportfolio/static/media/down-scroll.72cde581.svg"
+  },
+  {
+    "revision": "2ecb80db42c30fb77ff200b22606c141",
+    "url": "/eportfolio/static/media/logo-full.2ecb80db.svg"
+  },
+  {
+    "revision": "e9166ef2f8c07b54c76342d8c5e4920c",
+    "url": "/eportfolio/static/media/triangle.e9166ef2.svg"
+  }
+]);
