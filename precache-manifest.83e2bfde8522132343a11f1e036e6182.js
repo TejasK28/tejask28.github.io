89cc7cd439ab7aadb86fed7a37b7f9d334d51255
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-07-30 16:36:06 +0000

    Updates

---
+++ precache-manifest.83e2bfde8522132343a11f1e036e6182.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943c84dce51462b3de3b91d4286b4ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b444e9bda755b6a3bde",
+    "url": "/static/css/main.830b42dc.chunk.css"
+  },
+  {
+    "revision": "9ccf05479cf315b845f8",
+    "url": "/static/js/2.aba03444.chunk.js"
+  },
+  {
+    "revision": "b792366b0bf70b8910d3ca85f53b1654",
+    "url": "/static/js/2.aba03444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b444e9bda755b6a3bde",
+    "url": "/static/js/main.77fbdabe.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.77fbdabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bae3a2f7b388411b3e8",
+    "url": "/static/js/runtime-main.c3195428.js"
+  },
+  {
+    "revision": "72cde5813503aff2994bcdbd00dea887",
+    "url": "/static/media/down-scroll.72cde581.svg"
+  },
+  {
+    "revision": "2ecb80db42c30fb77ff200b22606c141",
+    "url": "/static/media/logo-full.2ecb80db.svg"
+  },
+  {
+    "revision": "e9166ef2f8c07b54c76342d8c5e4920c",
+    "url": "/static/media/triangle.e9166ef2.svg"
+  }
+]);
